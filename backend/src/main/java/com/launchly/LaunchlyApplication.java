dae3660a511f1e4644152eb60a49ae84de944ba2
polchduikt <polchilliakney@gmail.com>
2026-06-01 23:23:25 +0300

    initial: project structure

---
+++ backend/src/main/java/com/launchly/LaunchlyApplication.java
@@ -0,0 +1,13 @@
+package com.launchly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaunchlyApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/launchly/LaunchlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
